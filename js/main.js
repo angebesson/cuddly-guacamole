b3445afa156dd6b7955c365ac6846449a759f696
Angelina Besson <formadeltiempoange@gmail.com>
2024-07-18 14:07:19 -0300

    calculadora en dom por una pared

--- js/main.js
+++ js/main.js
@@ -1,59 +1,6 @@
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const button4 = document.querySelector("#button4");
-const button5 = document.querySelector("#button5");
-//Calculadora
-
-const paredText = document.querySelector("#paredText");
-const m2Text = document.querySelector("m2Text");
-const litrosText = document.querySelector("litrosText");
-
-
-//initialize buttons
-
-button3.onclick = calcularPintura;
-button4.innerHTML = pintura.color;
-button5.innerText = latasPintura[litros][1];
 
 
 //Funciones
-
-function calcularPintura() {
-    button3.innerText = "Go to town square";
-    let total = 0;
-      
-    for (let i = 1; i <= 4; i++) {
-
-        let ancho = parseFloat(prompt("Ingresa los metros de ancho de tu pared"));
-        let alto = parseFloat(prompt("Ingresa los metros de alto de tu pared"));
-        while (isNaN(ancho) || isNaN(alto)) {
-            alert('Debes ingresar un valor en números');
-            ancho = parseFloat(prompt("Ingresa los metros de ancho de tu pared"));
-            alto = parseFloat(prompt("Ingresa los metros de alto de tu pared"));
-                  }
-        let superficie = (ancho * alto);
-        let pintura = superficie / 12;
-        paredText.innerText = i++;
-        m2Text.innerHTML = superficie;
-        litrosText.innerText = pintura;
-        alert("Pared nro " + i);
-        alert("Superficie de tu pared es " + superficie + " metros cuadrados");
-        alert("Necesitas " + pintura + " litros de pintura por mano para cubrir tu pared");
-        total += pintura;
-        
- 
-    };
-    
-   
-    alert(" Necesitas un total de " + total + " litros de pintura POR MANO")
-}
-
-// crearCalculoPared(i,superficie,pintura){
-//     paredText.innerText = i;
-//     m2Text.innerHTML = superficie;
-//     litrosText.innerText = pintura;
-// }
 
 // function buyHealth() {
 //     if (gold >= 10) {
@@ -93,7 +40,7 @@ function calcularPintura() {
 //       text.innerText = "You already have the most powerful weapon!"
 //     }
 //   }
-  
+
 
 // function fightDragon() {
 //     fighting = 2; este es el indice
@@ -110,3 +57,34 @@ function calcularPintura() {
 //     goldText.innerText = gold;
 //     xpText.innerText = xp ;
 //   }
+
+//Usar esto para agregar a las paredes??
+// text.innerText += " Your "+inventory.pop()+" breaks."
+const text = document.querySelector("#text");
+const boton = document.getElementById("btn-calcular");
+text.innerHTML = "La superficie de tu pared es... "
+boton.addEventListener("click", () => {
+    // 	if (num_uno == "" || num_dos == ""){
+	// 	alert("Debes de Rellenar los campos");
+	// } else {
+    let total = 0;
+ for (let i = 1; i <= 3; i++) {
+    let alto = document.querySelector(".num_uno").value;
+    let ancho = document.querySelector(".num_dos").value;
+    let superficie = (ancho * alto);
+    let pintura = superficie / 12;
+      // alert("Pared nro " + i);
+    // alert("Superficie de tu pared es " + superficie + " metros cuadrados");
+    // alert("Necesitas " + pintura + " litros de pintura por mano para cubrir tu pared");
+    total += pintura;
+    text.innerText = "Cantidad de manos: " + i + "\n";
+    text.innerText += "La superficie de tu pared es "+ superficie + "  metros cuadrados " + "\n";
+    text.innerHTML += "Necesitas " + pintura + " litros de pintura por mano para cubrir tu pared" + "\n";
+} 
+// alert(" Necesitas un total de " + total + " litros de pintura ")
+text.innerHTML += " Necesitas un total de " + total + " litros de pintura";
+});
+
+
+//     alert(" Necesitas un total de " + total + " litros de pintura POR MANO")
+
