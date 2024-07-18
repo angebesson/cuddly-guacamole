afef10ddcb58602e4dc37c9d576088de4fa572ed
Angelina Besson <formadeltiempoange@gmail.com>
2024-07-18 17:51:44 -0300

    math ceil en total y borre coments que no iban

--- js/main.js
+++ js/main.js
@@ -2,64 +2,6 @@
 
 //Funciones
 
-// function buyHealth() {
-//     if (gold >= 10) {
-//       gold -= 10;
-//       health += 10;
-//       goldText.innerText = gold;
-//       healthText.innerText = health;
-//     } else {
-//       text.innerText = "You do not have enough gold to buy health.";
-//     }
-//   }
-// function buyWeapon() {
-//     if (gold >= 30) {
-//       gold -= 30;
-//       currentWeaponIndex++;
-//       goldText.innerText = gold;
-//       let newWeapon = weapons[currentWeaponIndex].name;
-//       text.innerText = "You now have a " + newWeapon + ".";
-//       inventory.push(newWeapon);
-//       text.innerText += " In your inventory you have: ";
-//     }
-//   }
-// function buyWeapon() {
-//     if (currentWeaponIndex < weapons.length - 1) {
-//       if (gold >= 30) {
-//         gold -= 30;
-//         currentWeaponIndex++;
-//         goldText.innerText = gold;
-//         let newWeapon = weapons[currentWeaponIndex].name;
-//         text.innerText = "You now have a " + newWeapon + ".";
-//         inventory.push(newWeapon);
-//         text.innerText += " In your inventory you have: " + inventory;
-//       } else {
-//         text.innerText = "You do not have enough gold to buy a weapon.";
-//       }
-//     }else{
-//       text.innerText = "You already have the most powerful weapon!"
-//     }
-//   }
-
-
-// function fightDragon() {
-//     fighting = 2; este es el indice
-//     goFight();
-//   }
-
-// Uso del objeto math
-//monsterHealth -= weapons[currentWeaponIndex].power +
-//Math.floor(Math.random() * xp) + 1; 
-
-// function defeatMonster() {
-//     gold += Math.floor(monsters[fighting].level * 6.7);
-//     xp += monsters[fighting].level;
-//     goldText.innerText = gold;
-//     xpText.innerText = xp ;
-//   }
-
-//Usar esto para agregar a las paredes??
-// text.innerText += " Your "+inventory.pop()+" breaks."
 const text = document.querySelector("#text");
 const boton = document.getElementById("btn-calcular");
 text.innerHTML = "La superficie de tu pared es... "
@@ -82,7 +24,7 @@ boton.addEventListener("click", () => {
     text.innerHTML += "Necesitas " + Math.ceil(pintura) + " litros de pintura por mano para cubrir tu pared" + "\n";
 } 
 // alert(" Necesitas un total de " + total + " litros de pintura ")
-text.innerHTML += " Necesitas un total de " + total + " litros de pintura";
+text.innerHTML += " Necesitas un total de " + Math.ceil(total) + " litros de pintura";
 });
 
 
