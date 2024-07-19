d8f97dd8d8930557bec04aa2137a512f38fe50a0
Angelina Besson <formadeltiempoange@gmail.com>
2024-07-19 13:18:08 -0300

    un poquito mas responsive y agregado de promo

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
 
 
-//Funciones
+//CALCULADORA
 
 const text = document.querySelector("#text");
 const boton = document.getElementById("btn-calcular");
+
 text.innerHTML = "La superficie de tu pared es... "
 boton.addEventListener("click", () => {
-    // 	if (num_uno == "" || num_dos == ""){
-	// 	alert("Debes de Rellenar los campos");
-	// } else {
+  let alto = document.querySelector(".num_uno").value;
+  let ancho = document.querySelector(".num_dos").value;
+    	if (alto == "" || ancho == ""){
+		alert("Debes ingresar los metros");
+	} else {
     let total = 0;
  for (let i = 1; i <= 3; i++) {
-    let alto = document.querySelector(".num_uno").value;
-    let ancho = document.querySelector(".num_dos").value;
-    let superficie = (ancho * alto);
+       let superficie = (ancho * alto);
     let pintura = superficie / 12;
       // alert("Pared nro " + i);
     // alert("Superficie de tu pared es " + superficie + " metros cuadrados");
     // alert("Necesitas " + pintura + " litros de pintura por mano para cubrir tu pared");
     total += pintura;
     text.innerText = "Cantidad de manos: " + i + "\n";
-    text.innerText += "La superficie de tu pared es "+ superficie + "  metros cuadrados " + "\n";
-    text.innerHTML += "Necesitas " + Math.ceil(pintura) + " litros de pintura por mano para cubrir tu pared" + "\n";
+    text.innerText += "La superficie de tu pared es "+ superficie + "  metros cuadrados. " + "\n";
+    text.innerHTML += "Necesitas " + Math.ceil(pintura) + " litros de pintura por mano para cubrir tu pared." + "\n";
 } 
 // alert(" Necesitas un total de " + total + " litros de pintura ")
-text.innerHTML += " Necesitas un total de " + Math.ceil(total) + " litros de pintura";
-});
+text.innerHTML += "\n" + " Necesitas un total de " + Math.ceil(total) + " litros de pintura";
+}});
 
 
 //     alert(" Necesitas un total de " + total + " litros de pintura POR MANO")
