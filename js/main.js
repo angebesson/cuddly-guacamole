99a8a07e3976f282a2752d67d4cc1132502fd312
Angelina Besson <formadeltiempoange@gmail.com>
2024-08-02 15:00:26 -0300

    Consulta a Api generador de color

--- js/main.js
+++ js/main.js
@@ -59,27 +59,26 @@ const search = document.getElementById("search");
 const resultadoPaleta = document.getElementById("resultadoPaleta")
 let color = search.value;
 let mode = entryDropdown.value;
+const resultadoColor = document.getElementById("resultadoColor")
 
 const getColorPalete = async (color, mode)=>{
   const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}`);
   const data = await response.json();
-  console.log (data);
   mostrarPaleta(data);
+  console.log(data)
 }
 
 const mostrarPaleta = (data)=> {
  const paleta = data.image.bare;
  const nombre = data.seed.name.value
- console.log(paleta);
  const div = document.createElement("div");
  div.classList.add("paletas");
-
  div.innerHTML = `
  <div class="contiene-colores">
     <h2>Resultado de tu búsqueda</h2>
     <img src="${paleta}" alt="paleta de colores relacionada">
      <h3>${nombre}</h3>
-    
+  
 </div>
  `
  resultadoPaleta.innerHTML = "";
@@ -93,10 +92,7 @@ formPaleta.addEventListener("submit", (e) => {
   e.preventDefault();
   let color = search.value;
   let mode = entryDropdown.value;
-  
-  console.log(color)
-  console.log(mode)
-  if(color){
+    if(color){
     getColorPalete(color,mode);
   }
   
@@ -104,14 +100,53 @@ formPaleta.addEventListener("submit", (e) => {
 
 //Acá para generar colores:
 
-// function getRandomHex() {
-//   var letters = '0123456789ABCDEF'.split('');
-//   var color = '#';
-//   for (var i = 0; i < 6; i++ ) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// }
+document.getElementById('getColor').addEventListener('click', function(evt){
+  evt.preventDefault;
+  let colorRandom = getRandomHex();
+  const h3 = document.createElement("h3");
+  h3.innerHTML = `<h3>Código de tu color: ${colorRandom}</h3>`
+  if(colorRandom){
+    getColor(colorRandom)}
+    resultadoCodigo.innerHTML = "";
+    if(resultadoCodigo){resultadoCodigo.appendChild(h3);};
+});
+
+function getRandomHex() {
+  let letters = '0123456789ABCDEF'.split('');
+  let color = '';
+  for (let i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+const getColor = async (colorRandom)=>{
+  const response = await fetch(`  https://www.thecolorapi.com/id?hex=${colorRandom}`);
+  const data = await response.json();
+  
+mostrarColor(data)
+}
+
+const mostrarColor = (data)=> {
+ const colorRand = data.image.bare;
+ const nombre = data.name.value
+ const div = document.createElement("div");
+ div.classList.add("colorRandom");
+ div.innerHTML = `
+ <div class="contiene-color">
+    <h2>Resultado de tu búsqueda:</h2>
+    <img src="${colorRand}" alt="color generado aleatorio">
+     <h3>${nombre}</h3>
+      
+</div>
+ `
+ resultadoColor.innerHTML = "";
+ if(resultadoColor){resultadoColor.appendChild(div);};
+
+}
+
+
 
 // https://www.thecolorapi.com/id?format=svg&hex=284614
 
